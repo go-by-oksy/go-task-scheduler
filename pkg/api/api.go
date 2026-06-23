c7ec3879a53c1f8263c9099ed2b1fb265853a1a0
Oksana <oklicorne1807@gmail.com>
2026-06-24 01:21:17 +0300

    Step 8

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -3,8 +3,9 @@ package api
 import "net/http"
 
 func Init() {
+	http.HandleFunc("/api/signin", signinHandler)
 	http.HandleFunc("/api/nextdate", nextDateHandler)
-	http.HandleFunc("/api/task", taskHandler)
-	http.HandleFunc("/api/tasks", tasksHandler)
-	http.HandleFunc("/api/task/done", doneTaskHandler)
+	http.HandleFunc("/api/task", auth(taskHandler))
+	http.HandleFunc("/api/tasks", auth(tasksHandler))
+	http.HandleFunc("/api/task/done", auth(doneTaskHandler))
 }
